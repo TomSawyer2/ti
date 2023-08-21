17e51945f8ace662a1735068f1bec26fbf4454d4
TomSawyer2 <6272867@qq.com>
2023-08-21 11:12:13 +0800

    🐳 chore: update files

---
+++ src/util/exception.ts
@@ -0,0 +1,19 @@
+import { error } from './log';
+
+/**
+ * 全局异常捕捉
+ *
+ * @param {*} callback
+ * @param {*} errCallback
+ */
+export async function exception(callback: () => Promise<void>, errCallback?: () => Promise<void>): Promise<void> {
+  try {
+    await callback();
+  } catch (err: any) {
+    error(err);
+    if (errCallback) await errCallback();
+    setTimeout(() => {
+      process.exit(-1);
+    }, 100);
+  }
+}
